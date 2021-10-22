d76e9bcbfedbca9bcf034075e6cf06e0f2aea063
RajkovskaMatea <63465726+RajkovskaMatea@users.noreply.github.com>
2021-10-22 18:51:42 +0200

    Add files via upload

---
+++ ddd-rentebike/shared-kernel/src/main/java/mk/ukim/finki/emt/sharedkernel/domain/financial/Money.java
@@ -0,0 +1,60 @@
+package mk.ukim.finki.emt.sharedkernel.domain.financial;
+
+import com.sun.istack.NotNull;
+import lombok.Getter;
+import mk.ukim.finki.emt.sharedkernel.domain.base.ValueObject;
+
+import javax.persistence.Embeddable;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import java.util.Objects;
+
+@Embeddable
+@Getter
+public class Money implements ValueObject {
+    @Enumerated(value = EnumType.STRING)
+    private final Currency currency;
+    //must be final so they are immutable
+    private final double amount;
+    protected Money(){
+        this.amount=0.0;
+        this.currency=null;
+    }
+    public Money(@NotNull Currency currency, @NotNull double amount){
+        this.currency=currency;
+        this.amount=amount;
+    }
+    //creates new instance from class Money
+    public static Money valueOf(Currency currency,int amount){return new Money(currency, amount);}
+    public Money add(Money money) {
+        if (!currency.equals(money.currency)) {
+            throw new IllegalArgumentException("Cannot add two Money objects with different currencies");
+        }
+        return new Money(currency,amount + money.amount);
+    }
+
+    public Money subtract(Money money) {
+        //we use method equals because we can not execute operations on momeny with different currency
+        if (!currency.equals(money.currency)) {
+            throw new IllegalArgumentException("Cannot add two Money objects with different currencies");
+        }
+        return new Money(currency,amount - money.amount);
+    }
+
+    public Money multiply(int m)  {
+        return new Money(currency,amount*m);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Money money = (Money) o;
+        return amount == money.amount && currency == money.currency;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(currency, amount);
+    }
+}
